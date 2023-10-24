8efb6ff72cae77c523fe0fd735157646dee4ade4
Chetan Rasik <chetanrasik7@gmail.com>
2023-10-24 20:52:32 +0530

    adding new repo

---
+++ trackingStatus.js/courierUrl.js
@@ -0,0 +1,112 @@
+export const courierUrls = {
+  "aact-freight-shipment-tracking":
+    "http://service.aact.co.kr/form/AACT/AACT_INQUERY.aspx?tabs=D&flag=ENG",
+  "asurion-shipment-tracking": "https://www.asurion.com/?from=",
+  "austrian-post-international-tracking":
+    "https://www.post.at/en/s/track-and-trace-search",
+  "babies-r-us-canada-tracking": "https://www.babiesrus.ca/en/trackorder",
+  "babies-r-us-australia-tracking": "https://www.babiesrus.ca/en/trackorder",
+  "bed-bath-and-beyond-order-tracking": "https://www.bedbathandbeyond.com/",
+  "belpost-belarus-tracking": "https://www.belpost.by/",
+  "bulgarian-posts-tracking": "https://www.bgpost.bg/en",
+  "costco-order-tracking": "https://freighttracking.costco.com/",
+  "cpc-canada-post-corporation-package-tracking": "",
+  "cyprus-post-tracking":
+    "https://www.canadapost-postescanada.ca/track-reperage/en",
+  "czech-republic-post-tracking": "https://www.postaonline.cz/en/trackandtrace",
+  "disney-store-order-tracking": "https://www.shopdisney.com/order-status",
+  "dji-order-tracking": "https://my.dji.com/orders",
+  "ebay-international-priority-mail-shipping-tracking":
+    "https://www.ebay.com/help/ship-track",
+  "emp-order-tracking": "https://www.emp.co.uk/",
+  "empost-uae-courier-tracking":
+    "https://www.emiratespost.ae/all-services/track-a-package",
+  "eub-shipping-carrier-tracking": "http://www.ems.com.cn/english.html",
+  "eyebuydirect-order-tracking": "https://www.eyebuydirect.com/order-tracking",
+  "fanatics-order-tracking": "https://www.fanatics.com/track-order",
+  "france-post-la-poste-tracking":
+    "https://www.laposte.fr/outils/track-a-parcel",
+  "frontier-communications-order-tracking":
+    "https://frontier.com/helpcenter/categories/order-status",
+  "ftd-flowers-order-tracking": "https://www.ftd.com/order-status",
+  "gap-order-tracking": "https://www.gap.com/customerService/info.do?cid=81266",
+  "ghana-post-tracking": "https://ghanapost.com.gh/",
+  "go-shop-order-tracking": "https://track.goshop.com.my/",
+  "indonesia-post-tracking": "https://ems.posindonesia.co.id/",
+  "interflora-order-tracking":
+    "https://www.interflora.co.uk/page/order-tracking",
+  "ips-courier-tracking": "http://ipsweb.ptcmysore.gov.in/ipswebtracking/",
+  "j-crew-factory-order-tracking":
+    "https://factory.jcrew.com/help/order-status",
+  "jcp-order-tracking": "https://www.jcpenney.com/orders/trackorders",
+  "jimmy-jazz-order-tracking": "http://www.jimmyjazz.com/track",
+  "justfab-order-tracking": "https://www.justfab.com/",
+  "kate-spade-order-tracking": "https://www.katespade.com/track-order",
+  "kfc-order-tracking": "https://www.kfc.tt/OrderTracker/GetOrderStatus",
+  "kylie-cosmetics-order-tracking":
+    "https://kyliecosmetics.com/en-in/pages/order-tracking",
+  "lakeside-collection-order-tracking": "https://www.lakeside.com/",
+  "lamps-plus-order-tracking":
+    "https://www.lampsplus.com/account/order-history/",
+  "latestone-online-shopping-order-tracking": "https://latestone.com/",
+  "lebanon-liban-post-ems-tracking":
+    "https://www.libanpost.com/english/tools-support/track-and-trace",
+  "lenscrafters-order-tracking":
+    "https://www.lenscrafters.com/lc-us/track-your-order",
+  "lycamobile-tracking": "https://www.lycamobile.se/en/track-order/",
+  "mac-cosmetics-tracking": "https://www.maccosmetics.com/track",
+  "magyar-posta-hungary-post-tracking":
+    "https://www.posta.hu/international_main",
+  "malta-post-ems-tracking": "https://www.maltapost.com/tracking",
+  "mc-order-tracking": "https://www.mcdelivery.co.in/more",
+  "mexico-post-tracking":
+    "https://www.portal.correosdemexico.com.mx/portal/index.php",
+  "misspap-order-tracking": "https://www.misspap.com/track-order",
+  "multiple-carriers-global-package-tracking": "",
+  "netflorist-order-tracking":
+    "https://m.netflorist.co.za/Mobile/Pages/MyAccount.aspx",
+  "new-zealand-post-tracking": "https://www.nzpost.co.nz/tools/tracking",
+  "nfl-shop-order-tracking": "https://www.nflshop.com/track-order",
+  "nigeria-nipost-tracking": "https://www.nipost.gov.ng/Track_Trace",
+  "osm-worldwide-shipping-tracking": "https://www.osmworldwide.com/tracking/",
+  "other-carrier-tracking": "https://www.walmart.com/orders",
+  "otterbox-order-tracking": "https://www.otterbox.com/en-us/order-search",
+  "pacsun-order-tracking":
+    "https://www.pacsun.com/help?a=Track-My-Order---id--Y7IGxdP0QWi74jeFS306vw",
+  "pakistan-post-tracking": "https://ep.gov.pk/track.asp",
+  "parcelnet-courier-tracking": "http://parcelnet.co.uk/",
+  "paypal-package-tracking":
+    "https://help.paypal.shipstation.com/hc/en-us/articles/360061609231-Track-a-Shipment",
+  "philippines-post-tracking": "https://tracking.phlpost.gov.ph/",
+  "pitney-bowes-parcel-tracking":
+    "https://www.pitneybowes.com/us/shipping-and-mailing/parcels-and-packages/package-tracking-solutions.html",
+  "poland-post-tracking": "https://emonitoring.poczta-polska.pl/?lang=en",
+  "pottery-barn-tracking":
+    "https://www.potterybarn.com/customer-service/order-shipment-tracking/",
+  "qatar-parcel-tracking": "https://qatarpost.qa/home",
+  "redbubble-order-tracking": "https://track.redbubble.com/",
+  "restoration-hardware-order-tracking":
+    "https://rh.com/content/category.jsp?context=YountvilleCart/Tracking",
+  "sailpost-italy-courier-tracking":
+    "https://www.sailpost.it/traccia-il-pacco/?q=tracker",
+  "scentsy-order-tracking": "https://scentsy.com/account/manage-orders",
+  "scrubs-and-beyond-tracking":
+    "https://www.scrubsandbeyond.com/sales/guest/form",
+  "seino-transportation-tracking":
+    "http://track.seino.co.jp/kamotsu/GempyoNoShokaiE.do",
+  "shoebuy-order-tracking": "https://www.dsw.com/",
+  "silver-sprint-courier-tracking": "http://www.silversprint.co.uk/tracking/",
+  "singpost-registered-mail-tracking": "https://www.singpost.com/track-items",
+  "soccer-com-order-tracking": "https://www.soccer.com/content/contact_us",
+  "sportsmans-guide-tracking": "https://www.sportsmansguide.com/",
+  "taiwan-post-tracking":
+    "https://postserv.post.gov.tw/pstmail/main_mail.html?targetTxn=EB500201",
+  "taxipost-tracking":
+    "https://www.posttrackings.com/taxipost-tracking-online/",
+  "teepublic-order-tracking": "https://www.teepublic.com/orders/lookup",
+  "tele2-tracking": "https://www.tele2.se/orderstatus.aspx",
+  "trackr-order-tracking": "https://parceltrackr.com/",
+  "usc-order-tracking":
+    "https://help.usc.co.uk/en/support/solutions/articles/80000495582-tracking-delivery",
+  "younique-order-tracking": "https://younique.aftership.com/",
+};
